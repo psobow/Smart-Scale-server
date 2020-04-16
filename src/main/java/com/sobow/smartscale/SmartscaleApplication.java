fcae5ddcdf023d582025bca2a15d13f7d6b4fca9
Patryk Sobow <patryk.sobow95@gmail.com>
2020-04-16 10:47:32 +0200

    Init project

---
+++ src/main/java/com/sobow/smartscale/SmartscaleApplication.java
@@ -0,0 +1,17 @@
+package com.sobow.smartscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartscaleApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(SmartscaleApplication.class, args);
+    // h2 database available under URL: http://localhost:8080/h2-console/
+    // config: JDBC URL = jdbc:h2:mem:testdb
+  }
+
+}
---
+++ src/test/java/com/sobow/smartscale/SmartscaleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sobow.smartscale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartscaleApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
